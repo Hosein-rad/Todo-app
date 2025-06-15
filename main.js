ccdd319320ae49b6c1be1b317b7694315a082c9a
Hosein-rad <160645426+Hosein-rad@users.noreply.github.com>
2025-06-15 19:36:30 +0330

    Update main.js

--- main.js
+++ main.js
@@ -10,8 +10,8 @@ const cancelBtn = document.getElementById("cancel-btn");
 const discardBtn = document.getElementById("discard-btn");
 const tasksContainer = document.getElementById("tasks-container");
 
-const taskData = [];
-const currentData = {};
+const taskData = JSON.parse(localStorage.getItem("data")) || [];
+let currentData = {};
 
 const reset = () => {
     titleInput.value = "";
@@ -19,12 +19,39 @@ const reset = () => {
     descriptionInput.value = "";
 };
 
+const deleteTask = (element) => {
+    element.parentElement.remove();
+    const itemToDelete = taskData.findIndex((item) => item.id === element.parentElement.id);
+    taskData.splice(itemToDelete, 1);
+};
+
+const editTask = (element) => {
+    const itemToEdit = taskData.findIndex((item) => item.id === element.parentElement.id);
+
+    titleInput.value = taskData[itemToEdit].title;
+    dateInput.value = taskData[itemToEdit].date;
+    descriptionInput.value = taskData[itemToEdit].description;
+    taskForm.classList.toggle("hidden");
+    addOrUpdateTaskBtn.innerText = "Update Task";
+
+    currentData.editItem = {
+        "id":  taskData[itemToEdit].id,
+        "title":  taskData[itemToEdit].title,
+        "date":  taskData[itemToEdit].date,
+        "description": taskData[itemToEdit].description,
+    }
+    taskData.splice(itemToEdit, 1);
+};
+
 openTaskFormBtn.addEventListener("click", () => {
+    addOrUpdateTaskBtn.innerText = "Add Task";
     taskForm.classList.toggle("hidden")
 });
 
 closeTaskFormBtn.addEventListener("click", () => {
-    confirmCloseDialog.showModal();
+    if (!titleInput.value && !dateInput.value && !descriptionInput.value) {
+        taskForm.classList.toggle("hidden");
+        } else {confirmCloseDialog.showModal()}
 });
 
 discardBtn.addEventListener("click", () => {
@@ -34,6 +61,7 @@ discardBtn.addEventListener("click", () => {
 
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
+    
     tasksContainer.innerHTML = "";
     const newObj = {
         id: `${titleInput.value}-${Date.now()}`,
@@ -43,13 +71,25 @@ taskForm.addEventListener("submit", (e) => {
     };  
     taskData.unshift(newObj);
     taskData.forEach((item) => {
-    tasksContainer.innerHTML += `<br/><p>Title: ${item.title}</p>
+    tasksContainer.innerHTML += `<div id="${item.id}"><p>Title: ${item.title}</p>
                                 <p>Date: ${item.date}</p>
                                 <p>Description: ${item.description}</p>
-                                <button type="button" class="btn">Edit</button>
-                                <button type="button" class="btn">Delete</button>
-                                <br/><hr/><br/>`;
+                                <button type="button" class="btn" onclick="editTask(this)">Edit</button>
+                                <button type="button" class="btn" onclick="deleteTask(this)">Delete</button>
+                                <hr/></div>`;
     });
     taskForm.classList.toggle("hidden");
     reset();
+    
 });
+if (currentData.editItem) {
+        taskData.unshift(currentData.editItem);
+
+        delete currentData.editItem;
+        
+    }; 
+
+
+// add localStorage
+// sth about currentData and taskData and adding/removing data between them is wrong
+// add regex to inputs
